3c91b86e737fe0c33f6ecd3ad7fc7f00b65798fe
sadegh-dev <sadegh.rez@gmail.com>
2023-02-04 19:20:22 +0330

    point number1 in models file

---
+++ models/important-models-points.py
@@ -0,0 +1,7 @@
+"""
+    After making changes in the fields of the model class, 
+    we must apply these changes in the db with the makemigrations command 
+    and then the migrate command, 
+    of course, after creating or making changes in the class methods, 
+    there is no need to execute the migrate commands.
+"""
