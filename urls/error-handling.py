1cbc126cff8a2f33ee0297c34a4427b194aa1e2f
sadegh-dev <sadegh.rez@gmail.com>
2023-02-09 01:08:03 +0330

    error-handling.py

---
+++ urls/error-handling.py
@@ -0,0 +1,9 @@
+"""
+    * If the called url is not in the list of url patterns, 
+    an error corresponding to that status will be returned.
+
+    * We can create these errors according to the following method:
+
+        https://docs.djangoproject.com/en/4.1/topics/http/views/#customizing-error-views
+
+"""
