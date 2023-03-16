b1fbdc5067a4d6c36a950f377678a2ebde8433f2
sadegh-dev <sadegh.rez@gmail.com>
2023-03-16 23:41:53 +0330

    function-path / connect to urls file in other app

--- urls/path-function.py
+++ urls/path-function.py
@@ -2,5 +2,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('welcome/', views.home, name='my-welcome')
+    path('welcome/', views.home, name='my-welcome') ,
+    path('welcome2/', views.Home2.as_view(), name='welcome-2') ,
+    path('weblog/', include('blog.urls', namespace='weblog'))
+    
 ]
