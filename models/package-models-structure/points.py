5de3f09f32052f9b5f83b315ee40549dc0ef7267
sadegh-dev <sadegh.rez@gmail.com>
2023-07-15 18:27:05 +0330

    package-models-structures #write Description

---
+++ models/package-models-structure/points.py
@@ -0,0 +1,19 @@
+"""
+    better way to avoid complexity
+
+    When there are many class-models of an app, 
+    it is better to use this way to avoid complexity.
+"""
+
+"""
+
+    In this way, we delete the models.py file in the app 
+    and create a directory called models, 
+    then we create the models class in separate files, 
+    finally we create a file named __init__ in that directory 
+    and all class-model We call it.
+"""
+
+
+
+
